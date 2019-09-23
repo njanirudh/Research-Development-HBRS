a0a5ea97de26733c4d07d1a9c3a7b1c168b92622
njanirudh <anijaya9@gmail.com>
2019-09-23 18:26:03 +0200

    Created .sh file

---
+++ src/frcnn.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --partition=gpu          # partition (queue)
+#SBATCH --ntasks-per-node=32     # number of cores per node
+#SBATCH --mem=64G                 # memory per node in MB (different units with suffix K|M|G|T)
+#SBATCH --time=2-00:00:00              # total runtime of job allocation (format D-HH:MM:SS; first parts optional)
+#SBATCH --output=frcnn_resnet_v1.%j.out    # filename for STDOUT (%N: nodename, %j: job-ID)
+#SBATCH --error=frcnn_resnet_v1.%j.err     # filename for STDERR
+
+# here comes the part with the description of the computational work, for example:
+# load the OpenMPI environment
+module load cuda
+
+cd /home/anaras2s/anirudh/Github/Tensorflow/models/research
+
+protoc object_detection/protos/*.proto --python_out=. 
+export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
+
+python /home/anaras2s/anirudh/Github/Tensorflow/models/research/object_detection/model_main.py --pipeline_config_path=/home/anaras2s/anirudh/Dataset/DoorDetect-Dataset/Model/faster_rcnn_inception_resnet_v2_atrous_oid_v4_2018_12_12/anirudh_pipeline.config --model_dir=/home/anaras2s/anirudh/Models/FRCNN --num_train_steps=100000 --sample_1_of_n_eval_examples=10 --alsologtostderr
